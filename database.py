aac84778bfe5a27fb363954fbb0e86fdc1bbeb58
Bana143123 <118828247+Bana143123@users.noreply.github.com>
2023-07-14 14:46:11 +0000

    updated databse credentials

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-dbconn_string = 'mysql+pymysql://2nq79l1l9j6dyddxiy5v:pscale_pw_c99IjpFOlNqt8Kjr3cSiDUNe356W8I3QrbTsir6vOuN@aws.connect.psdb.cloud/nina?charset=utf8mb4'
+dbconn_string = 'mysql+pymysql://2q35n140237o0y79mo1h:pscale_pw_leCtzoPjO5tBL81ncJUZbyL5uA0O6EXTa7Z6CkHR9vf@aws.connect.psdb.cloud/nina?charset=utf8mb4'
 engine = create_engine(
   dbconn_string,
   connect_args={"ssl": {
